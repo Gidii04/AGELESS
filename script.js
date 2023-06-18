f6eeb85e11d6f56610eee83dd87073d819d1749b
Atauba Gideon <86924003+Gidii04@users.noreply.github.com>
2023-06-18 19:41:14 +0100

    add all files

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ const now = new Date().getFullYear()
 console.log(now)
 
 date.innerHTML = now
+
+const mySearch = document.querySelector('#mysearch')
+const clearSearch = document.querySelector('.clear')
+clearSearch.addEventListener('click', () => {
+  mySearch.value = ''
+})
+
+const searchIcon = document.querySelector('.search-icon')
+const searchInput = document.querySelector('.input')
+const clearIcon = document.querySelector('.clear')
+
+searchIcon.addEventListener('click', () => {
+  searchIcon.classList.toggle('move')
+  searchInput.classList.toggle('show')
+  clearIcon.classList.toggle('show')
+})
